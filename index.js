1ba523466e7413d42f62586c9ae5938bfcfe3ac6
Luke Robertson <luke.robertson@korelogic.co.uk>
2018-05-12 16:10:15 +0100

    export fix

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const allQueryStrings = () =>
-	location.search ? location.search
-		.substr(1)
-		.split('&')
-		.reduce((acc, item) => {
-			const arr = item.split('=')
-			acc[arr[0]] = decodeURIComponent(arr[1])
-			return acc
-		}, {}) : {}
+  location.search
+    ? location.search
+        .substr(1)
+        .split('&')
+        .reduce(function(acc, item) {
+          const arr = item.split('=')
+          acc[arr[0]] = decodeURIComponent(arr[1])
+          return acc
+        }, {})
+    : {}
 
 const queryString = name => allQueryStrings()[name]
 
-export default { queryString, allQueryStrings }
+module.exports = { queryString, allQueryStrings }
