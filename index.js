56d5bfe12c974773a3df1909e9fe3a4daede4aee
Luke Robertson <luke.robertson@korelogic.co.uk>
2018-05-16 10:57:21 +0100

    updated to de-encode the key as well as the value

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const allQueryStrings = () =>
         .split('&')
         .reduce((acc, item) => {
           const arr = item.split('=')
-          acc[arr[0]] = decodeURIComponent(arr[1])
+          acc[decodeURIComponent(arr[0])] = decodeURIComponent(arr[1])
           return acc
         }, {})
     : {}
